79495f87357418de1c45f7fe49c9a08201c0b207
gauravmittal797-code <gauravmittal797@gmail.com>
2026-07-15 10:46:14 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,32 @@
-const CACHE = 'dukan-khata-v1';
+const CACHE_NAME = 'dukan-khata-v1';
+const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (event) => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(c => {
-      return c.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL).catch(()=>{}))
   );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => {
-      return r || fetch(e.request).catch(() => caches.match('./index.html'));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
     })
   );
 });
